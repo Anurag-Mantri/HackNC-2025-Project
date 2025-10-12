78f4bb6ce3992a3cd86631887dacbabec6922998
Sri Mantri <anuragmantri06@gmail.com>
2025-10-12 01:38:44 -0400

    Update ProjectHub.js

--- client/src/ProjectHub.js
+++ client/src/ProjectHub.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import CommunityTab from './components/CommunityTab';
 import './ProjectHub.css';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
