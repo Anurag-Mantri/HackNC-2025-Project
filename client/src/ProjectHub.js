aa36ead16fb7ac8a4704873f16112adb261f152a
Anurag-Mantri <anuragmantri06@gmail.com>
2025-10-11 23:39:58 -0400

    Attempting deployment

--- client/src/ProjectHub.js
+++ client/src/ProjectHub.js
@@ -4,7 +4,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import './ProjectHub.css';
 
-const API_URL = 'http://localhost:3001';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
